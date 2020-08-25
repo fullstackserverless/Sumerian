2d15a1f497d0d52a8820c377e4e18668fc2e6022
Playra <raoffonom@icloud.com>
2020-08-25 20:59:38 +0500

    fixed auth

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry, YellowBox } from 'react-native'
-import App from './src'
+import App from './src' // eslint-disable-line
 import { name as appName } from './app.json'
 
 YellowBox.ignoreWarnings([
